f1a16a2fb8a522d3579d3e7aa1603e3e60bb4322
Ajay-chitambaran <ajaychitambaran@gmail.com>
2018-11-07 23:22:03 +0530

    alex updated with lesser complexity for sai

--- alex.py
+++ alex.py
@@ -1,32 +1,17 @@
 #insert float values at prices
-origItems=[]
-origPrice=[]
-items=[]
-price=[]
-itindex=[]#address holder
-valuestorage=[]
-holder=[]
-holder2=[]
+owner={}#dictionary of price and item
 count=0
-n=int(input())
+n=int(input("Number of items going to enter:  "))
 for i in range(0,n):
-    origItems.append(input())#item array insertion
-    itindex.append(i)
-m=int(input())
-for k in range(0,m):
-    origPrice.append(float(input()))#price array insertion with index noticing
-    #prindex.append(k)#index value to an array
-search=int(input())
-for item in range(0,search):
-	itemcompar=input()
-	for j in range(0,len(itindex)):
-		if(itemcompar==origItems[itindex[j]]):
-			 holder.append(origPrice[itindex[j]])#moved to holder
-search2=int(input())
-if(search==search2):
-	for rep in range(0,search2):
-		nowval=float(input())
-		holder2.append(nowval)
-		if(holder[rep]!=holder2[rep]):
-			count=count+1
-print(count)#result
+    origItem=input("Enter orginal item:  ")
+    origPrice=float(input("Enter original price: "))
+    owner[origItem]=origPrice
+m=int(input("Number of items going to search: "))
+for j in range(0,m):
+    item=input("Enter item sold by alex: ")
+    price=float(input("Enter price sold by alex: "))
+    if(owner[item]!=price):
+        count=count+1
+    else:
+        count=count
+print("alex sold %d items incorrectly"%(count))
