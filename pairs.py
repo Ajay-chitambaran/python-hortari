c4f1570aa8b1662fc0dc07cdb5b33266d0f5d62e
Ajay-chitambaran <ajaychitambaran@gmail.com>
2018-11-08 14:03:10 +0530

    moving to windows

---
+++ pairs.py
@@ -0,0 +1,12 @@
+storage=[]
+count=0
+n=int(input())
+for i in range(0,n):
+    storage.append(int(input()))
+k=int(input())
+def numberofPairs(storage,k):
+    for j in range(0,len(storage)):
+        head=storage[j]
+        for k in range(0,len(storage)):
+            
+numberofPairs(storage,k)
